834f44c36b10cb555ea7532d993fc66b33b0070c
Nagataku <s1611455@u.tsukuba.ac.jp>
2018-10-19 16:43:23 +0900

    i can login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "sasakamaboko2018@gmail.com", #gmailアドレス
+    :password => "sasakama18", #gmailパスワード
+    :authentication => 'login',
+  }
+
 end
--- db/migrate/20181012072832_add_confirmable_to_devise.rb
+++ db/migrate/20181012072832_add_confirmable_to_devise.rb
@@ -4,7 +4,7 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.1]
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
-    # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+    #add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
     add_index :users, :confirmation_token, unique: true
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
@@ -15,6 +15,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.1]
 
   def down
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    # remove_columns :users, :unconfirmed_email # Only if using reconfirmable
+    #remove_columns :users, :unconfirmed_email # Only if using reconfirmable
   end
 end
