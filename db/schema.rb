c85dcaa59cbc4c4d5627e42200d78152bed49013
LisaHiyama <s1611457@u.tsukuba.ac.jp>
2018-10-26 16:21:43 +0900

    AddColumn_user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024043051) do
+ActiveRecord::Schema.define(version: 20181026071153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20181024043051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "easiness"
+    t.string "user_id"
     t.index ["lecture_id"], name: "index_user_voices_on_lecture_id"
   end
 
