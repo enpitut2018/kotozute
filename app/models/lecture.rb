b9d3ea81313f0097416d3376615f4c13d761a5fb
naokimatsudo <n.m.0201306@docomo.ne.jp>
2018-11-21 13:45:54 +0900

    add tag database

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,6 @@
 class Lecture < ApplicationRecord
     has_many :user_voices, dependent: :destroy
+    acts_as_taggable
     def self.csearch(name_search,module_search,grade_search,day_search,period_search) #self.でクラスメソッドとしている
         if name_search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
           Lecture.where(['class_name LIKE ?', "%#{name_search}%"]).where(['module LIKE ?', "%#{module_search}%"]).where(['grade LIKE ?', "%#{grade_search}%"])
