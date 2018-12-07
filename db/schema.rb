193016e99f743213d4349b01a34a1a5c9adddee6
LisaHiyama <38552750+LisaHiyama@users.noreply.github.com>
2018-12-07 15:45:34 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 20181121040153) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "user_name", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
